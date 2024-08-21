cbbd5464d4bc7ad6663eee0da4e48467a9053c23
Victoria Kruczek <victoria.kruczek@gmail.com>
2024-08-21 15:21:14 +0200

    A visitor can add a new restaurant, and be redirected to the show view of that new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,23 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create!(restaurant_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
