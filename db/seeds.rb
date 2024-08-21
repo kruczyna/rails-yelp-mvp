7d30a1da76d7b782ab3d14deb1196d7e83d69913
Victoria Kruczek <victoria.kruczek@gmail.com>
2024-08-21 14:56:11 +0200

    A visitor can see the list of all restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Destroy all existing records to avoid duplication
+Restaurant.destroy_all
+
+# Create sample restaurants
+restaurants = [
+  { name: 'Sushi Place', address: '123 Tokyo St', phone_number: '123-456-7890', category: 'japanese' },
+  { name: 'Pizza Heaven', address: '456 Rome Ave', phone_number: '098-765-4321', category: 'italian' },
+  { name: 'Waffles & More', address: '789 Brussels Rd', phone_number: '555-123-4567', category: 'belgian' },
+  { name: 'La Belle Vie', address: '321 Paris Blvd', phone_number: '321-654-9870', category: 'french' },
+  { name: 'Dragon’s Delight', address: '987 Beijing Ln', phone_number: '111-222-3333', category: 'chinese' }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
+puts "Created #{Restaurant.count} restaurants."
